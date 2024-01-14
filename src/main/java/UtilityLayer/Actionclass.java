ee96956d0c68028983c4b15f641cad6c568eed63
NILAMSONAWANE2830 <nilamsonawane3030@gmail.com>
2024-01-14 02:54:29 -0800

    action class modified by nilam

--- src/main/java/UtilityLayer/Actionclass.java
+++ src/main/java/UtilityLayer/Actionclass.java
@@ -1,5 +1,6 @@
 package UtilityLayer;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Wait;
@@ -27,6 +28,19 @@ public class Actionclass extends BaseClass{
 
 	public static void clickAndHold(WebElement wb) {
 		new Actions(driver).clickAndHold(Waits.visibilityStatus(wb)).build().perform();
+	}
+	public static void release(WebElement wb) {
+		new Actions(driver).release(Waits.visibilityStatus(wb)).build().perform();
+	}
+
+	public static void sendKeys(WebElement wb, String value) {
+		new Actions(driver).sendKeys(Waits.visibilityStatus(wb), value).build().perform();
+	}
+
+	public static void sendUCText(WebElement wb, String value) {
+
+		new Actions(driver).keyDown(Keys.SHIFT).sendKeys(Waits.visibilityStatus(wb), value).keyUp(Keys.SHIFT).build()
+				.perform();
 	}
 
 //	public static void dragAndDrop(WebElement source, WebElement destination) {
